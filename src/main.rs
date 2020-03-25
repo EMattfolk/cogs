9f83bf8d8822c74d6fa343f96ad77443820f3240
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-03-25 13:44:58 +0100

    Fix recursive functioncalls

--- src/main.rs
+++ src/main.rs
@@ -50,21 +50,25 @@ fn interpret_statement(line: &str) {
 
 fn interpret_expression(expr: &str) -> Box<dyn Object> {
     lazy_static! {
-        static ref FUNCTION_CALL_RE: Regex = Regex::new("([a-zA-Z_0-9-]+?)\\((.*?)\\)")
+        static ref FUNCTION_CALL_RE: Regex = Regex::new("([a-zA-Z_0-9-]+?)\\((.*)\\)")
             .unwrap();
         static ref STRING_RE: Regex = Regex::new("\"(.*?)\"|'(.*?)'").unwrap();
     }
 
     if FUNCTION_CALL_RE.is_match(expr) {
         let cap = FUNCTION_CALL_RE.captures(expr).unwrap();
+
         return interpret_functioncall(&cap[1], &cap[2]);
+
     } else if STRING_RE.is_match(expr) {
         let cap = STRING_RE.captures(expr).unwrap();
         let contents = cap.get(1).map_or("", |m| m.as_str()).to_string() +
             cap.get(2).map_or("", |m| m.as_str());
+
         return Box::new(CogString{data: contents});
+
     } else if expr == "" {
-        Box::new(CogNone{})
+        Box::new(CogNone)
     } else {
         panic!("Error parsing expression: '{}'", expr)
     }
@@ -76,12 +80,12 @@ fn interpret_functioncall(function: &str, args: &str) -> Box<dyn Object> {
         &_ => {}
     }
 
-    Box::new(CogNone{})
+    Box::new(CogNone)
 }
 
 fn main() {
     interpret_statement("// Comment");
-    interpret_statement("print()");
+    interpret_statement("print(print())");
     interpret_statement("print('')");
     interpret_statement("print(\"Hello world\")");
     interpret_statement("print('Hello world')");
